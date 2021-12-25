a26c932d7ba7f8fbca0e1ebace1298b6eba5cb2c
yzc1114 <34084331+yzchnb@users.noreply.github.com>
2021-12-25 13:21:13 +0800

    add report

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 		AppMsgs:                           []AppMsg{"AppMsg1"},                                          // 给出了程序传送哪些消息，是一个切片，程序间可以传输多个消息。按照题意，这里应该是1个消息。
 		AppProcessMainLoopIntervalBetween: []time.Duration{5 * time.Millisecond, 10 * time.Millisecond}, // 决定了程序运行的快慢
 	})
-	StartRoutines([]ProcessID{1, 2, 3, 4}, func(pid ProcessID, status ProcessStatus) bool {
+	StartRoutines([]ProcessID{1, 2, 3, 4, 5, 6, 7}, func(pid ProcessID, status ProcessStatus) bool {
 		if pid == 1 && (status == 101 || status == 201) {
 			return true
 		}
